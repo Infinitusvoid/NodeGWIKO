221444ce5d3dd9a5397a2f294630d3e4f335bf9d
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2024-04-15 21:49:32 +0200

    update

---
+++ NodeGWIKO_4in4out.py
@@ -0,0 +1,135 @@
+from pathlib import Path
+
+import os
+import numpy as np
+import torch
+from PIL import Image, ImageDraw, ImageFont
+import subprocess
+import json
+import base64
+
+def replace_string(main_string, target, replacement):
+    """
+    Replace all occurrences of 'target' in 'main_string' with 'replacement'.
+    
+    Parameters:
+    main_string (str): The original string.
+    target (str): The string to be replaced.
+    replacement (str): The string to replace 'target' with.
+    
+    Returns:
+    str: The modified string with replacements.
+    """
+    return main_string.replace(target, replacement)
+
+def create_folder_if_not_exists(folder_path):
+    """
+    Creates a folder if it doesn't exist already.
+
+    Args:
+    - folder_path (str): The path of the folder to be created.
+
+    Returns:
+    - None
+    """
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+def encode_to_base64(input_string):
+    # Convert the input string to bytes
+    input_bytes = input_string.encode('utf-8')
+    
+    # Encode the bytes to base64
+    encoded_bytes = base64.b64encode(input_bytes)
+    
+    # Convert the encoded bytes back to a string
+    encoded_string = encoded_bytes.decode('utf-8')
+    
+    return encoded_string
+
+# Function to save JSON to a file
+def save_json(data, file_path):
+    with open(file_path, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+    print("JSON data saved to:", file_path)
+
+def start_process_with_args(command, args):
+    try:
+        # Start the process with the given command and arguments
+        process = subprocess.Popen([command] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Wait for the process to finish and get its output
+        stdout, stderr = process.communicate()
+        
+        # Decode output bytes to strings
+        stdout_str = stdout.decode('utf-8')
+        stderr_str = stderr.decode('utf-8')
+        
+        # Check if there's any error output
+        if stderr_str:
+            print("Error occurred:", stderr_str)
+        else:
+            print("Process output:", stdout_str)
+    
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+
+def load_image(image_path):
+    try:
+        img = Image.open(image_path)
+        print("Image loaded successfully.")
+        return img
+    except FileNotFoundError:
+        print("Error: Image file not found.")
+        return None
+    except Exception as e:
+        print(f"Error: Failed to load image - {str(e)}")
+        return None
+
+def replace_slash(input_string):
+    return input_string.replace("/", "\\")
+
+class NodeGWIKO_4in4out:
+    def __init__(self):
+        pass
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "int_field": ("INT", {
+                    "default": 0, 
+                    "min": 0, #Minimum value
+                    "max": 4096, #Maximum value
+                    "step": 64, #Slider's step
+                    "display": "number" # Cosmetic only: display as "number" or "slider"
+                }),
+                "print_to_screen": (["enable", "disable"],),
+                "string_glsl_source": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "dynamicPrompts": False,
+                    "default": ""
+                }),
+            },
+            "optimal": {
+                "image_optimal_0":("IMAGE",),
+            }
+        }
+    
+    RETURN_TYPES = ("IMAGE",)
+    #RETURN_NAMES = ("image_output_name",)
+    
+    FUNCTION = "test"
+    
+    #OUTPUT_NODE = False
+
+    CATEGORY = "NodeGWIKO"
+    
+    def test(self, image, optimal, int_field, print_to_screen, string_glsl_source):
+        pass
--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .NodeGWIKO import NodeGWIKO
+from .NodeGWIKO_4in4out import NodeGWIKO_4in4out
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "NodeGWIKO": NodeGWIKO
+    "NodeGWIKO": NodeGWIKO,
+    "NodeGWIKO_4in4out":NodeGWIKO_4in4out
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "NodeGWIKO": "NodeGWIKO Node"
+    "NodeGWIKO": "NodeGWIKO Node",
+    "NodeGWIKO_4in4out":"NodeGWIKO_4in4out 4 in 4 out"
 }
